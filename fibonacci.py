6856b697baa900498e76debbf84edbb615a39fdb
Jelayo <jelayo@uoguelph.ca>
2025-09-30 11:40:21 -0400

    fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,6 +1,10 @@
-def fibonacci():
-  fib = input("How many terms of the Fibonacci sequence would you like?")
-    if fib <= 0:
-        return "Pick a number greater than 0"
-    else:
-        return "Figure this out"
+fib = input("How many terms of the Fibonacci sequence would you like?")
+if int(fib) <= 0:
+    print("Pick a number greater than 0")
+else:
+    x = 0
+    y = 1
+    z = 0
+    for z in range (int(fib)):
+        x, y = y, x + y
+        print(x)
