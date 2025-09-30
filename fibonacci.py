f2a72080d73661fd12c07ddbcfd91c2fd3c16058
Jelayo <jelayo@uoguelph.ca>
2025-09-30 10:41:11 -0400

    fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,10 +1,6 @@
-#!/usr/bin/env python3
-
-# Fibonacci Sequence Exercise
-# TODO: (Read detailed instructions in the Readme file)
-print("How many terms of the Fibonacci sequence would you like?")# Prompt the user for the number of terms.
-fib = input()# Validate that the input is a positive integer.
-if fib <= 0:# Use a for loop to print the Fibonacci sequence up to that many terms.
-  return "Pick a number greater than 0"
-else:
-  return "Figure this out"
+def fibonacci():
+  fib = input("How many terms of the Fibonacci sequence would you like?")
+    if fib <= 0:
+        return "Pick a number greater than 0"
+    else:
+        return "Figure this out"
